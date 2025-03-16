03b706aede8bb62567faaabf7cf26686a816733c
ablagoev11 <a.blagoev52@gmail.com>
2025-03-16 03:23:32 +0200

    Add message board project

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const indexRouter = require("./routers/index");
+
+app.use(express.urlencoded({ extended: true }));
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+app.use("/", indexRouter);
 
 const port = 3000;
 app.listen(port, () => {
--- routers/index.js
+++ routers/index.js
@@ -3,11 +3,13 @@ const indexRouter = Router();
 
 const messages = [
   {
+    id: 1,
     text: "Hi there!",
     user: "Amando",
     added: new Date(),
   },
   {
+    id: 2,
     text: "Hello World!",
     user: "Charles",
     added: new Date(),
@@ -15,8 +17,22 @@ const messages = [
 ];
 
 indexRouter.get("/", (req, res) => {
-  res.send("Hello, world!");
+  res.render("index", { title: "Mini Messageboard", messages: messages });
+});
+indexRouter.get("/new", (req, res) => {
+  res.render("form", {});
 });
 indexRouter.post("/new", (req, res) => {
-  res.send("About this site");
+  messages.push({
+    id: messages.length + 1,
+    text: req.body.message,
+    user: req.body.author,
+    added: new Date(),
+  });
+  res.redirect("/");
 });
+indexRouter.get("/message/:id", (req, res) => {
+  res.render("message", { message: messages[req.params.id - 1] });
+});
+
+module.exports = indexRouter;
